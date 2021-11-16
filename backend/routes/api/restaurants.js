4fc865e61c56910dc47b6b864cf7b6232d2e1f9f
BrandonMohan <barndonmo@gmail.com>
2021-11-15 20:32:26 -0500

    restaurant data displaying on / route

---
+++ backend/routes/api/restaurants.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const {Restaurant, User} = require("../../db/models")
+
+const router = express.Router();
+
+
+router.get('/', asyncHandler(async(req, res) => {
+    const restaurants = await Restaurant.findAll()
+    return res.json(restaurants)
+}));
+
+module.exports = router;
