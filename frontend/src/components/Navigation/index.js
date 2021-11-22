04eb796532a076f24ec710afadfb5a7ef6547e59
BrandonMohan <barndonmo@gmail.com>
2021-11-22 18:10:34 -0500

    working on touching up user auth and some styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,147 +1,150 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-import {showModal, setCurrentModal} from "../../store/modal"
+import { showModal, setCurrentModal } from "../../store/modal";
 import LoginFormPage from "../LoginFormPage";
 import SignupFormPage from "../SignupFormPage";
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
-import { searchRestaurants} from '../../store/search'
-
+import { searchRestaurants } from "../../store/search";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const [input, setInput] = useState("")
-  const searchResults = useSelector((state) => state.search)
-  const results = Object.values(searchResults)
-
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [input, setInput] = useState("");
+  const searchResults = useSelector((state) => state.search);
+  const results = Object.values(searchResults);
 
   const show = () => {
-      document.querySelector(".search-results").classList.remove("hidden")
+    document.querySelector(".results").classList.remove("hidden");
   };
 
   const hide = (e) => {
-      if (!e.currentTarget.contains(e.relatedTarget)) {
-          document.querySelector(".search-results").classList.add("hidden")
-      }
-  }
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      document.querySelector(".results").classList.add("hidden");
+    }
+  };
 
   const reset = () => {
-      document.querySelector(".search-results").classList.add("hidden")
-      setInput("");
-  }
-
-
-
+    document.querySelector(".results").classList.add("hidden");
+    setInput("");
+  };
 
   const handleLogin = (e) => {
     // e.preventDefault();
     dispatch(setCurrentModal(LoginFormPage));
     dispatch(showModal());
-};
+  };
 
   const handleSignup = (e) => {
-      dispatch(setCurrentModal(SignupFormPage));
-      dispatch(showModal());
-  }
+    dispatch(setCurrentModal(SignupFormPage));
+    dispatch(showModal());
+  };
 
   const handleHome = (e) => {
-      history.push('/home')
-  }
+    history.push("/home");
+  };
 
   useEffect(() => {
-      if (input.length > 0) {
-          console.log("input term", input);
-          dispatch(searchRestaurants(input))
-      }
-  }, [dispatch, input])
+    if (input.length > 0) {
+      console.log("input term", input);
+      dispatch(searchRestaurants(input));
+    }
+  }, [dispatch, input]);
 
   let sessionLinks;
   if (sessionUser) {
-        sessionLinks =
-        (
-           <div className="navContainer">
-
-    <div className="searchBar">
-            <div className="search-container"
-            onBlur={(e) => hide(e)}>
-              <input
-                className="search-bar"
-                value={input}
-                placeholder="Search"
-                onFocus={()=>show()}
-                onChange={(e) => setInput(e.target.value)}
-              />
-              <div className="search-results hidden">
-                  {results?.length > 0 && input.length > 0 ? (
-                      Object.values(results).map((res)=> (
-                          <NavLink
-                          className="search-card"
-                          onClick={reset}
-                          to={`/restaurants/${res.id}`}>
-                            <div className="search-name">{res.name}</div>
-                          </NavLink>
-                      ))
-                  ) : (
-                      <div className="search-none">No results</div>
-                  )
-                  }
-
-              </div>
+    sessionLinks = (
+      <div className="navContainer">
+        <div className="searchDiv">
+          <div className="searchContainer" onBlur={(e) => hide(e)}>
+            <input
+              className="searchBar"
+              value={input}
+              placeholder="Search"
+              onFocus={() => show()}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <div className="results hidden">
+              {results?.length > 0 && input.length > 0 ? (
+                Object.values(results).map((res) => (
+                  <NavLink
+                    className="searchCard"
+                    onClick={reset}
+                    to={`/restaurants/${res.id}`}
+                  >
+                    <div className="searchName">{res.name}</div>
+                  </NavLink>
+                ))
+              ) : (
+                <div className="noResults">No results found</div>
+              )}
             </div>
-
-
-
-      </div>
-      <div className="leftNav">
-             <ProfileButton className="profileBtn" user={sessionUser} />
-
-        <button className="homeBtn" onClick={handleHome}>Home</button>
+          </div>
         </div>
-    </div>
-    )
+        <div className="leftNav">
+          <button className="homeBtn" onClick={handleHome}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              height="1em"
+              width="1em"
+              style={{ transform: "rotate(360deg)" }}
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </button>
+          
+          <ProfileButton className="profileBtn" user={sessionUser} />
+
+        </div>
+      </div>
+    );
   } else {
     sessionLinks = (
       <div className="navContainer">
-      <div className="searchBar">
-            <div className="search-container"
-            onBlur={(e) => hide(e)}>
-              <input
-                className="search-bar"
-                value={input}
-                placeholder="Search"
-                onFocus={()=>show()}
-                onChange={(e) => setInput(e.target.value)}
-              />
-              <div className="search-results hidden">
-                  {results?.length > 0 && input.length > 0 ? (
-                      Object.values(results).map((res)=> (
-                          <NavLink
-                          className="search-card"
-                          onClick={reset}
-                          to={`/restaurants/${res.id}`}>
-                            <div className="search-name">{res.name}</div>
-                          </NavLink>
-                      ))
-                  ) : (
-                      <div className="search-none">No results</div>
-                  )
-                  }
-
-              </div>
+        <div className="searchBar">
+          <div className="search-container" onBlur={(e) => hide(e)}>
+            <input
+              className="search-bar"
+              value={input}
+              placeholder="Search"
+              onFocus={() => show()}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <div className="search-results hidden">
+              {results?.length > 0 && input.length > 0 ? (
+                Object.values(results).map((res) => (
+                  <NavLink
+                    className="search-card"
+                    onClick={reset}
+                    to={`/restaurants/${res.id}`}
+                  >
+                    <div className="search-name">{res.name}</div>
+                  </NavLink>
+                ))
+              ) : (
+                <div className="search-none">No results</div>
+              )}
             </div>
-
-
-
-      </div>
-      <div className="leftNav">
-        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
-        <button onClick={handleSignup}>SignUp</button>
-        <button onClick={handleHome}>Home</button>
+          </div>
+        </div>
+        <div className="leftNav">
+          <button className="btn btn-primary" onClick={handleLogin}>
+            Login
+          </button>
+          <button onClick={handleSignup}>SignUp</button>
         </div>
       </div>
     );
--- frontend/src/components/Restaurants/SingleRestaurant.js
+++ frontend/src/components/Restaurants/SingleRestaurant.js
@@ -1,103 +1,145 @@
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useParams } from 'react-router'
-import {loadOneRestaurant} from '../../store/singlerestaurant'
-import ConfirmModal from '../ConfirmModal'
-import './singlerestaurant.css'
-import { showModal, setCurrentModal } from '../../store/modal'
-import EditRestaurantForm from './UpdateRestaurantForm'
-import DeleteModal from './DeleteModal'
-import {allReviews, editOneReview } from '../../store/reviews'
-import { deleteReview } from '../../store/reviews'
-import CreateReviewForm from '../Reviews/CreateReviewForm'
-import UpdateReviewForm from '../Reviews/UpdateReviewForm'
-import { loadOneReview } from '../../store/singleReview'
-
-
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router";
+import { loadOneRestaurant } from "../../store/singlerestaurant";
+import ConfirmModal from "../ConfirmModal";
+import "./singlerestaurant.css";
+import { showModal, setCurrentModal } from "../../store/modal";
+import EditRestaurantForm from "./UpdateRestaurantForm";
+import DeleteModal from "./DeleteModal";
+import { allReviews, editOneReview } from "../../store/reviews";
+import { deleteReview } from "../../store/reviews";
+import CreateReviewForm from "../Reviews/CreateReviewForm";
+import UpdateReviewForm from "../Reviews/UpdateReviewForm";
+import { loadOneReview } from "../../store/singleReview";
 
 const SingleRestaurant = () => {
-    const { id } = useParams();
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.session.user)
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
 
+  useEffect(() => {
+    dispatch(loadOneRestaurant(id));
+    dispatch(allReviews(id));
+  }, [dispatch, id]);
 
+  const restaurant = useSelector((state) => state.singleRestaurant);
+  const reviews = useSelector((state) => Object.values(state.reviews));
+  const handleEdit = (e) => {
+    dispatch(setCurrentModal(EditRestaurantForm));
+    dispatch(showModal());
+  };
 
-    useEffect(() =>{
-        dispatch(loadOneRestaurant(id))
-        dispatch(allReviews(id))
-    }, [dispatch, id])
+  const handleDelete = (e) => {
+    dispatch(setCurrentModal(DeleteModal));
+    dispatch(showModal());
+  };
 
-    const restaurant = useSelector((state) => state.singleRestaurant)
-    const reviews = useSelector((state) => Object.values(state.reviews))
-    const handleEdit = (e) => {
-        dispatch(setCurrentModal(EditRestaurantForm))
-        dispatch(showModal())
-    }
+  const handleDeleteReview = (reviewId) => {
+    dispatch(deleteReview(reviewId));
+    dispatch(setCurrentModal(ConfirmModal));
+    dispatch(showModal());
+  };
 
-    const handleDelete = (e) => {
-        dispatch(setCurrentModal(DeleteModal))
-        dispatch(showModal())
-    }
+  const handleEditReview = (review) => {
+    dispatch(loadOneReview(review.id));
+    dispatch(setCurrentModal(UpdateReviewForm));
+    dispatch(showModal());
+  };
 
-    const handleDeleteReview = (reviewId) => {
-        dispatch(deleteReview(reviewId))
-        dispatch(setCurrentModal(ConfirmModal))
-        dispatch(showModal())
-    }
+  const handleCreateReview = (e) => {
+    dispatch(setCurrentModal(CreateReviewForm));
+    dispatch(showModal());
+  };
 
-    const handleEditReview = (review) => {
-        dispatch(loadOneReview(review.id))
-        dispatch(setCurrentModal(UpdateReviewForm))
-        dispatch(showModal())
-    }
+  const idArr = reviews.map((oneReview) => {
+    return oneReview.userId;
+  });
 
-    const handleCreateReview = (e) => {
-        dispatch(setCurrentModal(CreateReviewForm))
-        dispatch(showModal())
-    }
+  return (
+    <>
+      <div>
+        {idArr.includes(user.id) ? null : (
+          <button onClick={handleCreateReview}>Leave a Review</button>
+        )}
+        {user.id === restaurant.ownerId ? (
+          <button onClick={handleEdit}>Edit Restaurant</button>
+        ) : null}
+        {user.id === restaurant.ownerId ? (
+          <button onClick={handleDelete}>Delete</button>
+        ) : null}
+      </div>
+      <div className="SingleCard">
+        {restaurant?.name}
+        <br></br>
+        {restaurant.address}
+        <br></br>
+        {restaurant.city}
+        <br></br>
+        {restaurant.state}
+        <br></br>
+      </div>
 
-    const idArr = reviews.map((oneReview) => {
-       return oneReview.userId
-    })
+      <ul>
+        {reviews?.map((review) => {
+          return (
+            <li key={review.id} className="reviewclass">
+              {restaurant.name}
+              <br></br>
+              Review: {review.review}
+              <br></br>
+              Rating: {review.rating}/5
+              {user.id === review.userId ? (
+                <button
+                  type="button"
+                  onClick={() => handleDeleteReview(review.id)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    height="1em"
+                    width="1em"
+                    style={{ transform: "rotate(360deg)" }}
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
+                </button>
+              ) : null}
+              {user.id === review.userId ? (
+                <button type="button" onClick={() => handleEditReview(review)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    height="1em"
+                    width="1em"
+                    style={{ transform: "rotate(360deg)" }}
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                    />
+                  </svg>
+                </button>
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
-
-    return(
-        <>
-        <div>
-           { idArr.includes(user.id) ? null : <button onClick={handleCreateReview}>Leave a Review</button>}
-            { user.id === restaurant.ownerId ? <button onClick={handleEdit}>Edit Restaurant</button> : null}
-            { user.id === restaurant.ownerId ? <button onClick={handleDelete}>Delete</button> : null}
-        </div>
-        <div className="SingleCard">
-                {restaurant?.name}
-                <br></br>
-                {restaurant.address}
-                <br></br>
-                {restaurant.city}
-                <br></br>
-                {restaurant.state}
-                <br></br>
-        </div>
-
-        <ul>
-                    {reviews?.map((review) => {
-
-                        return(
-                         <li key={review.id} className="reviewclass">
-                             {restaurant.name}
-                             <br></br>
-                             Review: {review.review}
-                             <br></br>
-                             Rating: {review.rating}/5
-                             { user.id === review.userId ? <button type="button" onClick={()=>handleDeleteReview(review.id)}>Delete</button> : null}
-                             { user.id === review.userId ? <button type="button" onClick={()=>handleEditReview(review)}>Edit</button> : null}
-                             </li>
-                        )
-                    })}
-                </ul>
-
-        </>
-    )
-}
-
-export default SingleRestaurant
+export default SingleRestaurant;
